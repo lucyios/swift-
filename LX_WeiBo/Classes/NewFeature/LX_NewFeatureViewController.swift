7cf0dead83a3e7009f3c05cd52f6108f24f0d0ed
“lucy” <liluxinios@163.com>
2016-08-06 16:40:25 +0800

    开始按钮动画

--- LX_WeiBo/Classes/NewFeature/LX_NewFeatureViewController.swift
+++ LX_WeiBo/Classes/NewFeature/LX_NewFeatureViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import SnapKit
 
-class LX_NewFeatureViewController: UIViewController, UICollectionViewDataSource {
+
+class LX_NewFeatureViewController: UIViewController, UICollectionViewDataSource ,UICollectionViewDelegate{
 
     let maxCount = 4
     
@@ -27,9 +29,44 @@ class LX_NewFeatureViewController: UIViewController, UICollectionViewDataSource 
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("NewFeatureCellID", forIndexPath: indexPath) as! LXCollectionViewCell
         
         cell.index = indexPath.item
+        
+        cell.startButton.hidden = true
         return cell
         
     }
+    
+    // MARK - UICollectionViewDelegate
+    /**
+     * 当一个cell完全显示完就会调用
+     */
+    func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        
+        //1.获取当前展现在眼前的cell对应的cell
+        let path = collectionView.indexPathsForVisibleItems().last!
+        
+        //2.根据索引获取当前展现在眼前的cell 
+        let cell = collectionView.cellForItemAtIndexPath(path) as! LXCollectionViewCell
+        
+        //3.判断是否是最后一页
+        if path.item == maxCount - 1 {
+            cell.startButton.hidden = false
+            
+            cell.startButton.userInteractionEnabled = false
+            cell.startButton.transform = CGAffineTransformMakeScale(0.0, 0.0)
+            
+            //动画
+            // usingSpringWithDamping 的范围为 0.0f 到 1.0f ，数值越小「弹簧」的振动效果越明显。
+            // initialSpringVelocity 则表示初始的速度，数值越大一开始移动越快, 值得注意的是，初始速度取值较高而时间较短时，也会出现反弹情况。
+            UIView.animateWithDuration(2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 10.0, options: UIViewAnimationOptions(rawValue: 0), animations: {
+                cell.startButton.transform = CGAffineTransformIdentity
+                }, completion: { (_) in
+                    cell.startButton.userInteractionEnabled = true
+            })
+        }
+        
+    }
+    
+    
 }
 
 class LXCollectionViewCell: UICollectionViewCell {
@@ -58,18 +95,50 @@ class LXCollectionViewCell: UICollectionViewCell {
     private func setUpUI(){
         //添加子控件
         contentView.addSubview(iconView)
+        contentView.addSubview(startButton)
         //布局子控件
-        iconView.translatesAutoresizingMaskIntoConstraints = false
+//        iconView.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        var cons = NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[iconView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["iconView":iconView])
+//        
+//        cons += NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[iconView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["iconView": iconView])
+//        contentView.addConstraints(cons)
         
-        var cons = NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[iconView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["iconView":iconView])
+        //框架
+        iconView.snp_makeConstraints { (make) in
+//            make.left.equalTo(0)
+//            make.right.equalTo(0)
+//            make.top.equalTo(0)
+//            make.bottom.equalTo(0)
+            make.edges.equalTo(0)
+        }
         
-        cons += NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[iconView]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["iconView": iconView])
-        contentView.addConstraints(cons)
+        startButton.snp_makeConstraints { (make) in
+            make.centerX.equalTo(contentView)
+            make.bottom.equalTo(contentView.snp_bottom).offset(-160)
+        }
         
     }
     
+    
+    @objc private func startBtnClick(){
+        LXLog("")
+    }
+    
     // MARK - 懒加载
+    //大图容器
     private lazy var iconView = UIImageView()
+    //开始按钮
+    private lazy var startButton: UIButton = {
+        let btn = UIButton(type: UIButtonType.Custom)
+        btn.setBackgroundImage(UIImage(named: "new_feature_button"), forState: UIControlState.Normal)
+        btn.setBackgroundImage(UIImage(named: "new_feature_button_highlighted"), forState: UIControlState.Highlighted)
+        
+        btn.addTarget(self, action: #selector(LXCollectionViewCell.startBtnClick), forControlEvents: UIControlEvents.TouchUpInside)
+        return btn
+        
+    }()
+    
     
 }
 
@@ -90,15 +159,5 @@ class LXFlowLayout: UICollectionViewFlowLayout {
         
     }
 }
-
-
-
-
-
-
-
-
-
-
 
 
--- Podfile
+++ Podfile
@@ -2,3 +2,4 @@ use_frameworks!
 pod 'AFNetworking'
 pod 'SDWebImage'
 pod 'SVProgressHUD'
+pod 'SnapKit', '~> 0.15.0'
